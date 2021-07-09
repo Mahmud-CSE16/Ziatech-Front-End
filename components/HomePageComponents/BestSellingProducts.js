1e33ab1d4b75e4519bf12d3b29a18e2fbcf7ff9b
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-07-09 16:18:26 +0600

    categories added

--- components/HomePageComponents/BestSellingProducts.js
+++ components/HomePageComponents/BestSellingProducts.js
@@ -1,21 +1,14 @@
+import { bestSellingData } from "./bestSellingData";
+import Link from "next/link"
 
 const BestSellingProducts = () => {
     return (
         <>
             <section className="py-5">
-                <h2 className="text-center h3 font-bold">Top Selling Products of This Month</h2>
-                <div className="container mt-4">
-                    <div className="row featured-card">
-                        <div className="col-md-4">
-                            <FeaturedCard img="/img/products/01.jpeg" />
-                        </div>
-                        <div className="col-md-4">
-                            <FeaturedCard img="/img/products/02.jpeg" />
-                        </div>
-                        <div className="col-md-4">
-                            <FeaturedCard img="/img/products/04.jpeg" />
-                        </div>
-                        
+                <h2 className="text-center h3 font-bold">Top Product Categories</h2>
+                <div className="container featured-card mt-4">
+                    <div className="row  d-flex justify-content-around">
+                        {bestSellingData.map(item => <FeaturedCard data={item} key={item.id} />)}
                     </div>
                 </div>
             </section>
@@ -25,31 +18,34 @@ const BestSellingProducts = () => {
 
 export default BestSellingProducts;
 
-const FeaturedCard = ({img}) => {
-    const data = {
-        title: `Product name`,
-        description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
-        // img: `https://firebasestorage.googleapis.com/v0/b/ziatech-ltd.appspot.com/o/content-pixie-ZB4eQcNqVUs-unsplash.jpeg?alt=media&token=8a36b2e3-b1a4-4a2a-aff6-fcd2e91a69fe`
-        img: `https://firebasestorage.googleapis.com/v0/b/ziatech-ltd.appspot.com/o/james-ree-ZmeFtu11Hpc-unsplash.jpeg?alt=media&token=bb494bd2-d528-4f74-b90e-cdae60615f91`
-    }
+const FeaturedCard = ({ data }) => {
+    // const data = {
+    //     title: `Product name`,
+    //     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.`,
+    //     img: `https://firebasestorage.googleapis.com/v0/b/ziatech-ltd.appspot.com/o/james-ree-ZmeFtu11Hpc-unsplash.jpeg?alt=media&token=bb494bd2-d528-4f74-b90e-cdae60615f91`
+    // }
     return (
         <>
-            <figure>
-                <figcaption>
-                    <h2 className="">{data.title} </h2>
-                    <p>{data.description} </p>
-                </figcaption>
-            </figure>
-            <style jsx>
-                {`
+            <div className="col-md-3">
+                <Link href={`/products${data.link}`}>
+                    <figure>
+                        <figcaption>
+                            <h2 className="">{data.name} </h2>
+                            <p><a href="#" className="btn btn-danger">Visit Now</a> </p>
+                        </figcaption>
+                    </figure>
+                </Link>
+                <style jsx>
+                    {`
                 figure{
-                    background-image: linear-gradient(0deg, rgba(11, 40, 49, 0.08), #c1f3ff4d),url(${img});
+                    background-image: linear-gradient(0deg, rgba(11, 40, 49, 0.08), #c1f3ff4d),url(${data.img});
                 }
                 figure:hover{
-                    background-image: linear-gradient(0deg, rgba(11, 40, 49, 1), #0844539a),url(${img});
+                    background-image: linear-gradient(0deg, rgba(11, 40, 49, 1), #0844539a),url(${data.img});
                 }
                 `}
-            </style>
+                </style>
+            </div>
         </>
     )
 }
---
+++ components/HomePageComponents/bestSellingData.js
@@ -0,0 +1,73 @@
+export const bestSellingData = [
+    {
+        id: 1,
+        name: `Packaging & Printing Machine`,
+        description: ``,
+        img: `/products/categorized/01.jpeg`,
+        link: `/packaging-printing`
+    },
+    {
+        id: 2,
+        name: `Food & Beverage Machine`,
+        description: ``,
+        img: `/products/categorized/02.jpeg`,
+        link: `/food-beverage`
+    },
+    {
+        id: 3,
+        name: `Chemical & Cosmetic Machine`,
+        description: ``,
+        img: `/products/categorized/03.jpeg`,
+        link: `/chemical-cosmetics`
+    },
+    {
+        id: 4,
+        name: `Plastic & Rubber Machine`,
+        description: ``,
+        img: `/products/categorized/04.jpeg`,
+        link: `/plastic-rubber`
+    },
+    {
+        id: 5,
+        name: `Hardware & Others Machine`,
+        description: ``,
+        img: `/products/categorized/05.jpeg`,
+        link: `/hardware-others`
+    },
+    {
+        id: 6,
+        name: `Construction & Agriculture Machine`,
+        description: ``,
+        img: `/products/categorized/06.jpeg`,
+        link: `/construction-agriculture`
+    },
+    {
+        id: 7,
+        name: `Pharmaceutical & Herbal Machine`,
+        description: ``,
+        img: `/products/categorized/07.jpeg`,
+        link: `/pharmaceutical-herbal`
+    },
+    {
+        id: 8,
+        name: `Medical & Health Machine`,
+        description: ``,
+        img: `/products/categorized/08.jpeg`,
+        link: `/medical-health`
+    },
+    {
+        id: 9,
+        name: `Garments & Textile Machine`,
+        description: ``,
+        img: `/products/categorized/09.webp`,
+        link: `/garments-textiles`
+    },
+    {
+        id: 10,
+        name: `Leather & Leather Goods Machine`,
+        description: ``,
+        img: `/products/categorized/10.jpeg`,
+        link: `/leather-goods`
+    },
+
+]
