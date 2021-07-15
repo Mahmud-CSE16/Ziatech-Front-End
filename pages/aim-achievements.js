dd258c13ff14f79633a82e376b16600cc0b6f6b3
SHAHJAHAN MD. SWAJAN <swajan.mis13.du@gmail.com>
2021-07-15 20:04:35 +0600

    aim achievements page added

---
+++ pages/aim-achievements.js
@@ -0,0 +1,83 @@
+
+const AimAchievements = () => {
+    return (
+        <>
+            <section className="bg-dark py-5 text-white">
+                <h1 className="text-center">Aim & Achievements</h1>
+            </section>
+            <section className="py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 offset-md-2">
+                            <h2><strong>Our Aim:</strong></h2>
+
+                            <p>To provide Bangladeshi modern engineering technology in the every door step all upozilla, District &amp; Divisions of Bangladesh where the people will produce there own consumable &amp; day to day usable products. Where the Bangladesh will be changed radically.</p>
+
+                            <p>&nbsp;</p>
+
+                            <h2><strong>Achievement:</strong></h2>
+
+                            <p>To achieve business excellence and goal by justifying consumers/ customers expectations through quality products. Also establishing a congenial working place with competent employees and by offering quality products at a reasonable price. In the slogan of &ldquo; A Stair of Top Success &rdquo; ziatech auto machine &amp; engineering company ltd wants a leading engineering company with in a short span of time. This company has supplied more than 100 products (various products) to the clients in different districts of the country. Al over, we are planning to export the machineries primarily in the SAARC countries.</p>
+
+                            <p>&nbsp;</p>
+
+                            <h2><strong>Career Opportunity:</strong></h2>
+
+                            <p>We offer challenging professional opportunity to the persons who can give different lime and focused hand on performance in the field of marketing, chemist, technician, production, IT &amp; Engineering. Vacancies arise from time to time to fulfill the requirements of the company.</p>
+
+                            <p>We offer attractive salaries and benefits in lime with top industrial employers in Bangladesh scope of advancement is strictly performance based. Presently, Chemist, Engineer, Technicians of food, beverage, chemical, cosmetic, plastic, rubber, construction, agriculture, pharmaceutical, herbal, packaging, garments &amp; textile. Several vos of post is ready to fill up. Send your CV and directly communicate with Executive Director- 01719-279470.</p>
+
+                            <p>&nbsp;</p>
+
+                            <p>&nbsp;</p>
+
+                            <h2><u>Our Aim:</u></h2>
+
+                            <p>Our company ziatech auto machine &amp; engineering company ltd. is an old &amp; experienced company of Bangladesh. Naturally, we have a definite goal or aim which we want to achieve step by step.</p>
+
+                            <h2><u>Our prime aim:</u></h2>
+
+                            <p>We have declared that our prime aim or goal is to develop our country as an <strong>industrial developed Bangladesh </strong>by manufacturing &amp; setting Bangladeshi machineries and steel products whose are made by ziatech. ziatech makes these by using ultra modern engineering technology.</p>
+
+                            <p>To form industrial developed Bangladesh we are forming our all cities as industrial cities and we are forming our all villages as industrial villages and we are forming our people as an industrialist by supplying ziatech machineries and steel products in every division, district, upozilla and village of whole Bangladesh. Our industrious people will produce there own consumable &amp; day to day usable industrial products by using ziatech machineries and steel products. In this way we will be able to develop our Bangladesh as an industrial developed Bangladesh.</p>
+
+                            <h2><u>Our second aim:</u></h2>
+
+                            <p>We have defined our aim as second aim that our ultra modern technological machineries and steel products will export in the SAARC countries or nearest countries of Bangladesh. In this way we will be able to earn foreign currency for our Bangladesh and this currency will be helpful for developing our country as an industrial developed Bangladesh. Already we are working to fulfill our second aim.</p>
+
+                            <h2><u>Our final aim:</u></h2>
+
+                            <p>Our final goal or aim is that we shall supply our ultra modern technological machineries and steel products in all over the world. The whole world will know about our technology and effort by reading &ldquo;Made in Bangladesh&rdquo; and &ldquo;Manufactured by ziatech auto machine &amp; engineering company ltd.&rdquo;. We will be able to earn foreign currency from whole world for our Bangladesh. The whole world will know and respect our dear country as an industrial developed Bangladesh.</p>
+
+                            <p>&nbsp;</p>
+
+                            <h2><strong>Our Achievement:</strong></h2>
+
+                            <p>We have achieved within the last 8 years that-</p>
+
+                            <ol>
+                                <li>&nbsp;&ldquo;ziatech&rdquo; is well known at home &amp; abroad for the best technology, machineries, service, honesty, experience, skill and for our helpful website- www.ziatechbd.com.</li>
+                                <li>We have set up near about 100 industries where so many kinds of industrial products are manufacturing by using our &ldquo;ziatech&rdquo; machineries &amp; products.</li>
+                                <li>So many unskilled or skilled or unemployed people have formed as entrepreneurs or industrialists whose are playing important role in our national economy to form our country as a developed country as well as they have created the way of employment for many unemployed people of the country.</li>
+                                <li>By strictly ensuring the quality standards, ziatech achieved ISO 9001:2014 Certification, which is a reflection of its uncompromising attitude towards quality. ziatech is the first organization in the industry achieving the ISO certificate in this category for Bangladesh.</li>
+                                <li>Also ziatech has gained so many awards from different organization for the successful contributions of manufacturing and supplying ultra modern technological machineries and steel products.</li>
+                                <li>Finally, as a whole we have able to establish that &ldquo;ziatech&rdquo; is &ldquo;A Stair of Top Success&rdquo;.</li>
+                            </ol>
+
+                            <p>&nbsp;</p>
+
+                            <h2><strong>Career Opportunity:</strong></h2>
+
+                            <p>We offer challenging professional opportunity to the persons who can give different lime and focused hand on performance in the field of marketing, chemist, technician, production, IT &amp; Engineering. Vacancies arise from time to time to fulfill the requirements of the company.</p>
+
+                            <p>We offer attractive salaries and benefits in lime with top industrial employers in Bangladesh scope of advancement is strictly performance based. Presently, Chemist, Engineer, Technicians of food, beverage, chemical, cosmetic, plastic, rubber, construction, agriculture, pharmaceutical, herbal, packaging, garments &amp; textile. Several vos of post is ready to fill up. Send your CV and directly communicate with Executive Director- 01719-279470.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default AimAchievements;
